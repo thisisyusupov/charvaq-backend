2946f1d77f8a11912d8243d1fc7afcba16813cd6
Rashidbek Raximov <rashidbekraximov21@gmail.com>
2023-05-29 22:15:08 +0500

    initial

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,18 @@
 package com.example.demo;
 
+import com.example.demo.config.OpenApiProperties;
+import com.example.demo.config.ServerProperties;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.CrossOrigin;
+
+@EnableConfigurationProperties({
+	OpenApiProperties.class,
+	ServerProperties.class
+})
 
 
 @SpringBootApplication
